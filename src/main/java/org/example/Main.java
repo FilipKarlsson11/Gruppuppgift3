72c1deab5212c484efc05b99a553c7fb8ac2e3e7
FilipKarlsson11 <Filip.karlsson11@hotmail.com>
2024-11-04 14:42:12 +0100

    Fjärde

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main3(String[] args) {
+    public static void main(String[] args) {
 
         try{
             Class<?> clazz = Class.forName("Calculator");
-            Constructor<?> clazzConstructor =  clazz.getDeclaredConstructor(int.class);
-            clazzConstructor .setAccessible(true);
-            Calculator calculator2 = (Calculator) clazzConstructor.newInstance(1);
+            Calculator calculator = (Calculator) clazz.getDeclaredConstructor().newInstance(1);
+            //Constructor<?> clazzConstructor =  clazz.getDeclaredConstructor(int.class);
+            //clazzConstructor .setAccessible(true);
+            //Calculator calculator2 = (Calculator) clazzConstructor.newInstance(1);
+            //Add add = clazz.getDeclaredMethod(add, 1);
+            //add.setAccessible(true);
+            calculator.toString();
+
+            //Class clazz = Class.forName("Calculator");
+        
         } catch (Exception e) {
             e.printStackTrace();
         }
