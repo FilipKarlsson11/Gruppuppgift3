e640c6aea7c8c9ba622ae8ee18202b38f8507ae3
FilipKarlsson11 <Filip.karlsson11@hotmail.com>
2024-11-04 14:21:49 +0100

    Guppgift3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Calculator {
+
+        private int number;
+
+
+        private Calculator(int number) {
+
+            this.number = numbers;
+
+        }
+
+
+        private int add(int other) {
+
+            return this.number + other;
+
+        }
+
+    }
+}
